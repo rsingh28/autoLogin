c6a671b76d0e3eb1ce2b7af776be62f158e6bec9
rsingh28 <rashmeet.singh28@gmail.com>
2017-11-27 20:04:16 -0500

    Automating Google Search

--- testingAutomationLibs/seleniumTesting.py
+++ testingAutomationLibs/seleniumTesting.py
@@ -1,6 +1,10 @@
 import time
 import sys
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Array of arguments (Not starting from 0 since thats the name of the script)
+arguments = sys.argv[1:]
 
 def googleSearch():
 
@@ -26,13 +30,48 @@ def googleSearch():
 	# Press the button
 	btn.click()
 
-	user_choice = raw_input('Please click ENTER button to close application')
+	user_choice = raw_input('Please click ENTER button to close script')
 	if not user_choice:
 		print("ABORTED")
 		quit()
 
-googleSearch() 
+def googleSearchAuto():
 
-	
+	driver = webdriver.Chrome()
+
+	driver.get("http://google.com")
+
+	driver.set_page_load_timeout(10)
+
+	input = driver.find_element_by_name("q")
+
+	input.send_keys(arguments[0])
+
+	print(arguments[0])
+
+	btn = driver.find_element_by_name("btnK")
+
+	# Alternate of btn.click() - Press Enter on the button instead
+	btn.send_keys(Keys.ENTER)
+
+	user_choice = raw_input('Please click ENTER button to close scipt')
+	if not user_choice:
+		print("ABORTED")
+		quit()
+
+def main():
+
+	if not arguments:
+
+		print("Doing default search since no argument is specified")
+
+		googleSearch()
+
+	else:
+
+		print("Doing a google search for - " + arguments[0])
+
+		googleSearchAuto()
 
 
+main()
